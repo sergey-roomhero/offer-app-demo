59668bd108b63370d61b0d5870519e65e6a67630
Sergey Sudilovskii <137996574+sergey-roomhero@users.noreply.github.com>
2024-04-03 10:19:11 +0000

    Import JSON

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+// Import JSON Data
+async function fetchJSON(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("JSON loading error: ${response.status}`);
+  }
+  return await response.json();
+}
+
+const clientsDataPromise = fetchJSON("data/clients.json");
+const productsDataPromise = fetchJSON("data/products.json");
+const ordersDataPromise = fetchJSON("data/orders.json");
+
+Promise.all([clientsDataPromise, productsDataPromise, ordersDataPromise])
+  .then(([clientsData, productsData, ordersData]) => {
+    console.log("Clients Data:", clientsData);
+    console.log("Products Data:", productsData);
+    console.log("Orders Data:", ordersData);
+
+  })
+  .catch((error) => {
+    console.error("Произошла ошибка при загрузке данных:", error);
+  });
